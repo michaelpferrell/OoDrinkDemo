1f2878a3070697899e1cf42a862cca2360756697
Mike Ferrell <mikeferrell.neo@gmail.com>
2023-03-24 11:58:21 -0400

    * Method Test_Drink_New_Carb.Test_IsCarbonated now tests nullness of the drink object first. * Add new testing class Test_Drink_New_NonCarb (same file), which tests the same things as Test_Drink_New_Carb except for a noncarbonated drink.

--- OoDrinkDemoDotNet/TestOoDrinkDemoData/Test_Drink.cs
+++ OoDrinkDemoDotNet/TestOoDrinkDemoData/Test_Drink.cs
@@ -28,7 +28,46 @@ namespace TestOoDrinkDemoData
         [TestMethod]
         public void Test_IsCarbonated()
         {
-            Assert.IsTrue(_TestedDrink?.IsCarbonated);
+            Assert.IsNotNull(_TestedDrink);
+            Assert.AreEqual(_IsCarbonated, _TestedDrink?.IsCarbonated);
+        }
+
+        [TestMethod]
+        public void Test_Description()
+        {
+            string? CachedDescription = _TestedDrink?.Description;
+            Assert.IsNotNull(CachedDescription);
+            Assert.AreEqual(_ExpectedDescription, CachedDescription);
+        }
+    }
+
+
+    [TestClass]
+    public class Test_Drink_New_NonCarb
+    {
+        private const string _Name = "Some Noncarbonated Drink";
+        private const bool _IsCarbonated = false;
+        private const string _ExpectedDescription = _Name + ", not carbonated.";
+
+        private Drink? _TestedDrink = null;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _TestedDrink = new Drink(_Name, _IsCarbonated);
+        }
+
+        [TestMethod]
+        public void Test_Name()
+        {
+            Assert.IsNotNull(_TestedDrink?.Name);
+            Assert.AreEqual(_Name, _TestedDrink?.Name);
+        }
+
+        [TestMethod]
+        public void Test_IsCarbonated()
+        {
+            Assert.IsNotNull(_TestedDrink);
             Assert.AreEqual(_IsCarbonated, _TestedDrink?.IsCarbonated);
         }
 
